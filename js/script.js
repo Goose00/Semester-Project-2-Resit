55fd4ce9c95fdbc40d9627a01c74d07555f692f9
Jan Jan20PT <57501781+Goose00@users.noreply.github.com>
2023-08-13 22:34:59 +0200

    layout and responsiveness

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// You don't need the smooth scrolling script for navigation links
+// because the default browser behavior will navigate to separate HTML files.
+
+// Leave this script file empty or use it for other functionalities you might need.
